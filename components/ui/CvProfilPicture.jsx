448147fba5f7bc8396635e8e5b6009b1aa36adc3
AurelienHouse <aurelien.maison@gmail.com>
2024-07-24 23:17:12 +0200

    💄 avatar

--- components/ui/CvProfilPicture.jsx
+++ components/ui/CvProfilPicture.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 
-import React from 'react';
+import React from "react";
 
-const CvProfilPicture = () => {
-  return (
-    <img src="/cv-picture-0724.png" alt="ma photo de profil" />
-  );
+const CvProfilPicture = ({ className }) => {
+  return <img src="/cv-picture-0724.jpg" alt="ma photo de profil" />;
 };
 
 export default CvProfilPicture;
